0c8e2cb2f1310b90cf1f5dd0b8ddba2ca2ea1ac9
LForchini <leonardo.forchini@gmail.com>
2021-12-22 10:05:34 +0000

    Added initial tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,63 @@
 import App from "./App";
-import { render } from "@testing-library/react";
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
+import { act } from "react-dom/test-utils";
+import { render, unmountComponentAtNode } from "react-dom";
 
-test("renders success message", () => {
+let container = null;
 
+beforeEach(() => {
+  container = document.createElement("div");
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test("calculator starts off at 0", () => {
+  render(<App />, container);
+
+  expect(container.querySelector(".text-input").value).toBe("0");
+});
+
+test("buttons clear on first press", () => {
+  render(<App />, container);
+
+  const button = container.querySelector(".btn-1");
+  button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+
+  expect(container.querySelector(".text-input").value).toBe("1");
+});
+
+test("all buttons can be pressed without errors", () => {
+  render(<App />, container);
+
+  const char_buttons = [
+    "clr",
+    "del",
+    "o-bracket",
+    "c-bracket",
+    "plus",
+    "minus",
+    "multiply",
+    "divide",
+    "equals",
+  ];
+
+  char_buttons.forEach((label, i) => {
+    let button = container.querySelector(`.btn-${label}`);
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  for (let i = 0; i < 10; i++) {
+    let button = container.querySelector(`.btn-${i}`);
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  }
+
+  let button = container.querySelector(".btn-clr");
+  button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+
+  expect(container.querySelector(".text-input").value).toBe("0");
 });
